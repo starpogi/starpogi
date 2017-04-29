4b7e818b88e26fe1407dd1d029d96a8edb337c5a
Javier Onglao <javier@juicero.com>
2017-04-29 15:38:29 -0700

    Get stuff to work

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask_compress import Compress
+
+app = Flask(__name__)
+Compress(app)
+
+@app.route('/')
+def index():
+    return "Hello"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
