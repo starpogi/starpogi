f444586cebe30629a6eaf35ca733358f5148fa8b
Javier Onglao <javier@juicero.com>
2017-04-29 17:24:44 -0700

    Challenge auth

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,11 @@ Compress(app)
 def index():
     return "Hello"
 
+
+@app.route('.well-known/acme-challenge/vZrl32ZOApJXtEgbIxOJmj9DQ7b32EJWAucsA8-GdO4')
+def cert_auth_challenege():
+    return "vZrl32ZOApJXtEgbIxOJmj9DQ7b32EJWAucsA8-GdO4.aFBw64AccQoHNKnhqbHOMUdaKJxtW7Ny4oe9DELEn5w"
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
